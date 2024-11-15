55935b5d378788977bf784929b0cb8a1b6569872
YoselinMA <162406301+YoselinMA@users.noreply.github.com>
2024-11-14 20:14:58 -0600

    Update Problemario2_2.sql

--- Problemario2_2.sql
+++ Problemario2_2.sql
@@ -1,1 +1,24 @@
+## Ejercicios UPDATE
 
+##1. **Actualiza el precio de todos los productos de una categoria aumentando un 15%.**  
+   
+##*Instrucción:* Usa la columna `categoria` para filtrar los productos.
+
+UPDATE productos 
+SET precio = precio * 1.15 
+WHERE categoria = 'Electrónica';
+
+##2. **Modifica el correo de uno de los clientes por un nuevo correo electrónico.**
+   
+##*Instrucción:* Asegúrate de que el nuevo correo sea único.
+
+UPDATE clientes 
+SET correo = 'nuevo.email@example.com' 
+WHERE cliente_id = 2;
+
+##3. **Corrige el stock de un producto cuyo stock actual es incorrecto.** 
+##*Instrucción:* Busca el producto por su `producto_id` y actualiza el campo `stock`.
+
+UPDATE productos 
+SET stock = 25 
+WHERE producto_id = 3;
